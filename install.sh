42adb30dfd2ef6dd22a01d359047292fbea34d63
Yaohan Chen <yaohan.chen@gmail.com>
2015-04-16 21:00:58 -0400

    Install python script as executable

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 install -D -m755 hib-dlagent           "$DEST/usr/bin/hib-dlagent"
 install -D -m644 README                "$DEST/usr/share/doc/hib-dlagent/README"
-install -D -m644 discover-url.py       "$DEST/usr/share/hib-dlagent/discover-url.py"
+install -D -m755 discover-url.py       "$DEST/usr/share/hib-dlagent/discover-url.py"
 
