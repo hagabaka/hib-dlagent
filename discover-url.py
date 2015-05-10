7095ec0950e8a2b968020eb0552a21c3325a08fa
Yaohan Chen <yaohan.chen@gmail.com>
2015-05-10 19:46:41 -0400

    Avoid errors when API returns None instead of []

--- discover-url.py
+++ discover-url.py
@@ -13,9 +13,9 @@ client.login(username, password)
 
 order_list = client.order_list()
 for order in order_list:
-    for subproduct in order.subproducts:
-        for download in subproduct.downloads:
-            for download_struct in download.download_struct:
+    for subproduct in order.subproducts or []:
+        for download in subproduct.downloads or []:
+            for download_struct in download.download_struct or []:
                 url = download_struct.url.web
                 if url and filename in url:
                     print(url)
