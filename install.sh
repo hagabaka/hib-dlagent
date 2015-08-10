4074902dc6a2267119e5ed32543ec02e1f15b863
Hugo Osvaldo Barrera <hugo@barrera.io>
2015-08-10 15:03:43 -0300

    Provide a Makefile to install (and uninstall)

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-install -D -m755 hib-dlagent           "$DEST/usr/bin/hib-dlagent"
-install -D -m644 README                "$DEST/usr/share/doc/hib-dlagent/README"
-install -D -m755 discover-url.py       "$DEST/usr/share/hib-dlagent/discover-url.py"
-
